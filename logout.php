6891a626e1c305de047ad4d0f96470bfa476db5b
Bill MacKenty <bmackenty@users.noreply.github.com>
2018-12-10 09:40:32 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,11 +2,11 @@
 // This file logs out a user.
 // in order to destroy a session we must first open one!
 // this page will redirect a user to login.php
+// This file should be named logout.php
 
 session_start();
-
+session_unset();
 session_destroy();
 
 header('location:login.php');
 ?>
-

d3064fa9e561b56f894758dd7ad26fbf85849127
Bill MacKenty <bmackenty@users.noreply.github.com>
2018-12-12 14:00:50 +0100

    cleaned up placeholdrs and margins

--- register.php
+++ register.php
@@ -36,13 +36,13 @@ include('header.php');
 
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Please enter your email address">
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
 
   <div class="form-group">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Please enter a password">
   </div>
 
   <button type="submit" class="btn btn-primary">Click here to register</button>

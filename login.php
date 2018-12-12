85b7d46d51ccfc8e575c8948c7af5eb447d97ae2
Bill MacKenty <bmackenty@users.noreply.github.com>
2018-12-12 10:19:22 +0100

    added sign up link

--- login.php
+++ login.php
@@ -1,85 +1,58 @@
-<!doctype html>
-<html lang="en">
-    <!-- Documentation: http://getbootstrap.com/docs/4.1/getting-started/introduction/ -->
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!-- start nav bar -->
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                  <ul class="navbar-nav">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                  </ul>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+                  <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                    <?php 
+                      session_start();
+                      $logged_in = $_SESSION['logged_in'];
+                      if ($logged_in == False) {
+                    ?>
 
-    <title>Hello, world!</title>
-  </head>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="register.php">Sign up</a>
+                    </li>
+                      <a class="nav-link" href="login.php">Login</a>
+                    <?php
+                      } else {
+                        $email = $_SESSION['email'];
+                    ?>
+                        <a class="nav-link" href="#">You are logged in as
+                        <?php echo $email; ?> </a>
+                        
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="logout.php">Logout</a>
+                        </li>
 
-  <!-- keep your content within body opening and closing tags -->
-  <body>
+                    <?php
+                      }
+                    ?>
+                    
+                    
+                    </li>
+                  </ul>
+                </div>
+        </nav>
+      <!-- end nav bar -->
       
-<?php 
-include('header.php');
-?>
-
-<div class="container m-3"> <!-- open container -->
-
-<?php 
-session_start();
-
-$logged_in = $_SESSION['logged_in'];
-if ($logged_in == False) {
-?>  
-
-<form action="login_process.php" method="post">
-
-  <div class="form-group">
-
-<?php 
-  if ($_SESSION['wrong_password']) {
-    echo "<strong>The password was incorrect. Try again</strong><br />";
-    unset($_SESSION['wrong_password']);
-  } elseif ($_SESSION['error_no_email']) {
-    echo "<strong>We can't find a user with that email address</strong><br />";
-    unset($_SESSION['error_no_email']);
-  }
-?>
-
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-
-<?php
-} else { 
-  $email = $_SESSION['email'];
-  echo "You are already logged in as $email! ";
-  ?>
-  <a href="logout.php">Click here to logout</a>
-  <?php
-}
-?>
-
-</div>  <!-- close container -->
-
-
-
-
-    <!-- Please don't touch anything below this line -->
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-  </body>
-
-<?php
-include('footer.php');
-?>
-
-</html>

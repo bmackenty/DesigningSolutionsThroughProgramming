859974eab0e4304298595e609a11e59fb990d564
Bill MacKenty <bmackenty@users.noreply.github.com>
2019-01-08 11:15:18 +0100

    added logic for passwords

--- user_edit_process.php
+++ user_edit_process.php
@@ -3,19 +3,26 @@
 session_start();
 include('database_inc.php');
 
-// we should santize this stuff below
+// we should santize the stuf below:
 
 $email = $_POST['email'];
 $username = $_POST['username'];
 $id_to_edit = $_SESSION['id_to_edit'];
+$password = $_POST['password'];
 
-// we are going to ignore password for right now
-// because there is a weird thing about it
-// $password = $_POST['password'];
+if(!empty($password)) {
+    $safe_password = password_hash($password,PASSWORD_DEFAULT);
 
-$result = mysqli_query($connect,"UPDATE users SET email = '$email', username = '$username' 
-WHERE id like $id_to_edit;");
+    $result = mysqli_query($connect,"UPDATE users 
+    SET email = '$email', username = '$username', password='$safe_password' 
+    WHERE id like $id_to_edit;");
+
+} else { 
+
+    $result = mysqli_query($connect,"UPDATE users 
+    SET email = '$email', username = '$username'
+    WHERE id like $id_to_edit;");
+}
 
 header('location:users.php');
-
 ?>

052fdc541b77ebae536e375573b067b576a60a33
Bill MacKenty <bmackenty@users.noreply.github.com>
2019-01-10 11:30:51 +0100

    added roles

--- user_edit_process.php
+++ user_edit_process.php
@@ -3,24 +3,31 @@
 session_start();
 include('database_inc.php');
 
-// we should santize the stuf below:
-
 $email = $_POST['email'];
 $username = $_POST['username'];
 $id_to_edit = $_SESSION['id_to_edit'];
 $password = $_POST['password'];
+$role = $_POST['role'];
+
+// Even though this page should only be used by an admin
+// it's probably a good idea to sanitize
+
+$email = mysqli_real_escape_string($connect,$email);
+$username = mysqli_real_escape_string($connect,$username);
+$role = mysqli_real_escape_string($connect,$role);
+
 
 if(!empty($password)) {
     $safe_password = password_hash($password,PASSWORD_DEFAULT);
 
     $result = mysqli_query($connect,"UPDATE users 
-    SET email = '$email', username = '$username', password='$safe_password' 
+    SET email = '$email', username = '$username', password='$safe_password', role='$role' 
     WHERE id like $id_to_edit;");
 
 } else { 
 
     $result = mysqli_query($connect,"UPDATE users 
-    SET email = '$email', username = '$username'
+    SET email = '$email', username = '$username', role = '$role'
     WHERE id like $id_to_edit;");
 }
 

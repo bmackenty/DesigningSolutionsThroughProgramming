36fcdff5e6d7479131d094651216dafbfeb0f697
Bill MacKenty <bmackenty@users.noreply.github.com>
2019-12-09 10:32:40 +0100

    Update store_navbar.php

--- store_navbar.php
+++ store_navbar.php
@@ -1,4 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<!-- this file should be named store_navbar.php --> 
         <a class="navbar-brand" href="inventory.php">ASW School Store</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>

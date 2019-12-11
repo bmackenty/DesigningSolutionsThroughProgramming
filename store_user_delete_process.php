17fe0bfdc4496e132ca16602b864d335b6a7e8f6
Bill MacKenty <bmackenty@users.noreply.github.com>
2019-12-11 09:44:36 +0100

    Create store_user_delete_process.php

---
+++ store_user_delete_process.php
@@ -0,0 +1,13 @@
+<?php 
+
+// this file should be named store_user_delete_process.php
+// there are many security problems with this file, which will be fixed in future versions
+
+$user_id_to_delete = $_GET['id'];
+
+include('database_inc.php');
+
+$result = mysqli_query($connect,"DELETE FROM users WHERE id = '$user_id_to_delete';");
+header('location:store_edit_users.php');
+
+?>

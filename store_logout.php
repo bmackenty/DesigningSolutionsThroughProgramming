c62bba755a6c27be3cc78ea45ae351e12bc6660a
Bill MacKenty <bmackenty@users.noreply.github.com>
2020-01-07 15:20:47 +0100

    Update store_logout.php

--- store_logout.php
+++ store_logout.php
@@ -11,6 +11,6 @@ session_start();
     setcookie(session_name(), '', 0, $params['path'], $params['domain'], $params['secure'], isset($params['httponly']));
     session_unset();
     session_destroy();
-}
+
 header('location:store_login.php');
 ?>

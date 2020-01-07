d1d13b9c4032e44d4c73b5c41dcfaf409d78ee8a
Bill MacKenty <bmackenty@users.noreply.github.com>
2020-01-07 18:26:26 +0100

    Update store_navbar.php

--- store_navbar.php
+++ store_navbar.php
@@ -14,12 +14,31 @@
                 </li>
             </ul>
             <ul class="navbar-nav">
+                <?php
+                session_start();
+                $logged_in = $_SESSION['logged_in'];
+                $email = $_SESSION['logged_in_user'];
+                if($logged_in == True) {
+                    ?>
+                    <li class="nav-item">
+                        <a class="nav-link"><?php echo $email; ?></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="store_logout.php">Logout</a>
+                    </li>
+                <?php
+                } else {
+                    ?>
                 <li class="nav-item">
                     <a class="nav-link" href="store_login.php">Login</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="store_register.php">Register</a>
                 </li>
+                    <?php
+                }
+                ?>
+
             </ul>
         </div>
     </nav>

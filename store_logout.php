db9bc51559994dfe2b717164d26f5f66964e8a52
Bill MacKenty <bmackenty@users.noreply.github.com>
2020-01-07 15:19:45 +0100

    Create store_logout.php

---
+++ store_logout.php
@@ -0,0 +1,16 @@
+<?php
+
+// This file logs out a user.
+// in order to destroy a session we must first open one!
+// this page will redirect a user to index.php after loggin out. 
+// This file should be named logout.php
+
+session_start();
+
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', 0, $params['path'], $params['domain'], $params['secure'], isset($params['httponly']));
+    session_unset();
+    session_destroy();
+}
+header('location:store_login.php');
+?>

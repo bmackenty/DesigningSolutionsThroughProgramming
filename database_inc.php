dbe7cbfae731c38ab631120a6ce286a568505fb9
Bill MacKenty <bmackenty@users.noreply.github.com>
2020-09-20 18:32:03 +0200

    Create database_inc.php

---
+++ database_inc.php
@@ -0,0 +1,19 @@
+<?php
+
+// this file should be named database_inc.php
+//
+// this file is used to open a connection to your database.
+// Anytime you need to connect to a database, you must include this file. 
+// 
+// to setup this file, please find the email I sent with your account information (not moodle)
+// you need to replace YOURUSERNAME with you username. You need to do this twice
+// you need to replace YOURPASSWORD with your password. You only need to do this once
+
+$connect = mysqli_connect("localhost","YOURUSERNAME","YOURPASSWORD","YOURUSERNAME");
+
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>

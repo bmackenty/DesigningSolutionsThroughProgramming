891ad6df23045b66c252e53dea0f72612b769a86
Bill MacKenty <bmackenty@users.noreply.github.com>
2020-09-20 21:14:01 +0200

    Update store_add_new_item_process.php

--- store_add_new_item_process.php
+++ store_add_new_item_process.php
@@ -16,6 +16,5 @@ $result = mysqli_query($connect,
     VALUES ('$quantity', '$size', '$description','$category','$image','$price');");
 
 
+header('location:store_control_panel.php');
 ?>
-
-<a href="inventory.php">Click here to go back to inventory</a>

5944bd3b93b999ea6b0ea9c992a925909c66fb97
Bill MacKenty <bmackenty@users.noreply.github.com>
2020-10-08 11:14:02 +0200

    um, oops. This time I'm serious. adding a cart actually works now

--- store_setup.php
+++ store_setup.php
@@ -82,8 +82,8 @@ if (!$connect) { ?>
     <!-- =========================================== -->
 
     <?php 
-$query_test_user_table = mysqli_query($connect, "select 1 FROM cart LIMIT 1;");
-if($query_test_user_table !== FALSE)
+$query_test_cart_table = mysqli_query($connect, "select 1 FROM cart LIMIT 1;");
+if($query_test_cart_table !== FALSE)
 { ?>
     <div class="alert alert-success" role="alert">
     You have a <strong>'cart'</strong> table in your database. That's good. 
@@ -91,12 +91,12 @@ if($query_test_user_table !== FALSE)
 <?php }
 else
 {
-    $sql_create_users = "CREATE TABLE cart (
+    $sql_create_cart = "CREATE TABLE cart (
         id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         item_id TEXT NULL,
         user_id TEXT NULL
         )";
-    mysqli_query($connect, $sql_create_users); ?>
+    mysqli_query($connect, $sql_create_cart); ?>
         <div class="alert alert-success" role="alert">
     You didn't have a <strong>'cart'</strong> table in your database, but I've created one for you.  
     </div>
@@ -564,7 +564,7 @@ $count_query = mysqli_query($connect, "SELECT COUNT(id) AS total FROM items;");
 while ($row = mysqli_fetch_assoc($count_query)){
     $total_items =  $row['total'];
 }
-if ($total_items > 6) { ?>
+if ($total_items > 5) { ?>
     <div class="alert alert-success" role="alert">
     You have at least 6 items in your items table, which is a good thing. 
     </div>

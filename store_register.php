7c7744fe8ae7b3441b1faf9fa4b43aedadd72d0f
Bill MacKenty <bmackenty@users.noreply.github.com>
2021-09-23 19:02:55 +0200

    update to bootstrap 5

--- store_register.php
+++ store_register.php
@@ -1,61 +1,50 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-  </head>
-<body>
-    <!-- =========================================== -->
-    <!-- PLEASE DO NOT EDIT ANYTHING ABOVE THIS LINE -->
-    <!-- =========================================== -->
-<?php include('store_navbar.php'); ?>
-<div class="container mt-5">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+        <title>Welcome to our store</title>
+    </head>
+    <body>
+
+    <?php 
+        include('store_navbar.php'); 
+    ?>
+        <div class="container">
   
   <div class="row">
     <div class="col-12 text-center">
       <h2>Thank you for registering</h2>
     </div>
   </div>
-      <div class="alert alert-primary alert-dismissible fade show" role="alert">
-        <strong>Thank you</strong> for registering. We will need to create your account.
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-        </button>
-    </div>
+
+        <div class="alert alert-primary alert-dismissible fade show" role="alert">
+        <strong>Thank you</strong> For registering. Please be patient while we activate your account. 
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+
     <form action="store_register_process.php" method="POST" class="mb-5">
         <div class="form-group">
             <label for="exampleInputEmail1">Email address</label>
-            <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Please enter your email address">
             <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
         <div class="form-group">
             <label for="exampleInputPassword1">Password</label>
-            <input name="password" type="password" class="form-control" id="exampleInputPassword1">
+            <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Please enter your password. Make it a good password!">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary mt-3">Submit</button>
     </form>
   
   
-  
-  
-  
-  
-    </div> <!-- close the container -->
-    <footer class="footer mt-auto mt-4 py-3 bg-secondary text-white text-center">
-      <div class="container">
-        <span >Thank you for visiting our site. :-) </span>
-      </div>
-    </footer>
-    <!-- =========================================== -->
-    <!-- PLEASE DO NOT EDIT ANYTHING BELOW THIS LINE -->
-    <!-- =========================================== -->
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-  </body>
+
+    </div> <!-- closing container div --> 
+<?php 
+    include('store_footer.php');
+?>
+<!-- please don't touch anything below this line --> 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</body>
 </html>
+

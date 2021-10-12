8df6c9f41e0dc0aa5f2de08a1ecfa3dcafcdce5e
Bill MacKenty <bmackenty@users.noreply.github.com>
2021-10-12 07:55:04 +0200

    Update store_edit_item.php

--- store_edit_item.php
+++ store_edit_item.php
@@ -1,65 +1,57 @@
-
 <!doctype html>
-<!-- this file should be named store_edit_item.php -->
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-  </head>
-<body>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+        <title>Welcome to our store</title>
+    </head>
+    <body>
     <!-- =========================================== -->
     <!-- PLEASE DO NOT EDIT ANYTHING ABOVE THIS LINE -->
     <!-- =========================================== -->
     <?php include('store_navbar.php'); ?>
-<div class="container mt-5">
+        <div class="container mt-5">
 
-<table class="table table-hover table-bordered table-striped">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Description</th>
-      <th scope="col">Size</th>
-      <th scope="col">Cost</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-<tbody>
-  
-  
-  <?php
+        <table class="table table-hover table-bordered table-striped">
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col">Description</th>
+            <th scope="col">Size</th>
+            <th scope="col">Cost</th>
+            <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        
+        
+        <?php
 
-include('database_inc.php');
+        include('database_inc.php');
 
-$result = mysqli_query($connect,
-"SELECT * FROM items;");
+        $result = mysqli_query($connect,
+        "SELECT * FROM items;");
 
-while ($row = mysqli_fetch_array($result))
-{
-?>
-    <tr>
-      <th scope="row"><?php echo $row['id']; ?></th>
-      <td><?php echo $row['description']; ?></td>
-      <td><?php echo $row['size']; ?></td>
-      <td><?php echo $row['price']; ?></td>
-      <td><a href="store_edit_item_process.php?id=<?php echo $row['id']; ?>">Edit this item</a></td>
-    </tr>
-  <?php
-}
-?>
-  </tbody>
-</table>
-</div> <!-- close the container -->
-    
-    <!-- =========================================== -->
-    <!-- PLEASE DO NOT EDIT ANYTHING BELOW THIS LINE -->
-    <!-- =========================================== -->
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-  </body>
+        while ($row = mysqli_fetch_array($result))
+        {
+        ?>
+            <tr>
+            <th scope="row"><?php echo $row['id']; ?></th>
+            <td><?php echo $row['description']; ?></td>
+            <td><?php echo $row['size']; ?></td>
+            <td><?php echo $row['price']; ?></td>
+            <td><a href="store_edit_item_process.php?id=<?php echo $row['id']; ?>">Edit this item</a></td>
+            </tr>
+        <?php
+        }
+        ?>
+        </tbody>
+        </table>
+</div> <!-- closing container div --> 
+        <?php 
+        include('store_footer.php');
+        ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    </body>
 </html>

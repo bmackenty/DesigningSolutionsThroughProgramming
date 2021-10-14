350be1acf5fe37cd55b3c49a80f33323748a0956
Bill MacKenty <bmackenty@users.noreply.github.com>
2021-10-14 10:41:59 +0200

    Update store_login_process.php

--- store_login_process.php
+++ store_login_process.php
@@ -1,50 +1,29 @@
-<?php
-session_start();
+<?php 
 // This file should be named store_login_process.php
-// below we include our database connection
-
+session_start();
 include('database_inc.php');
 
-// the 2 lines below capture the data sent from a form.
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-// the lines below execute a QUERY to the database. In this case, 
-// we SELECT everything from the table 'users'
-// we store the query into a variable named "$result"
-$result = mysqli_query($connect,
-"SELECT * FROM users WHERE email LIKE '$email';");
-
-// the line below tests if our database query found any results. 
-if (mysqli_num_rows($result) == 0) {
-  $_SESSION['error_no_email'] = True;
-  header('location:store_login.php');
-
-} else {
-
-// the loop below iterates through the results (if there are any)
-// we use the actual colum names from our database in the results. 
-// the column names need to match exactly to the column names in your database
-while ($row = mysqli_fetch_array($result))
-{
-  $password_in_databases = $row['password'];
-
-  if (password_verify($password,$password_in_databases)) {
-
-    // this conditional is only true is the password which was entered in our form 
-    // is the same as the encypted password stored in our database. 
-    $logged_in_user_id = $row['id'];
-    $_SESSION['logged_in'] = True;
-    $_SESSION['logged_in_user'] = $email;
-    $_SESSION['logged_in_user_id'] = $logged_in_user_id;
-    header('location:store_control_panel.php');
-
-  } else {
-
-    // this conditional is true if the password did NOT match. 
-    $_SESSION['error_wrong_password'] = True;
+ 
+$query_users = mysqli_query($connect, "SELECT * FROM users WHERE email LIKE '$email';");
+if(mysqli_num_rows($query_users) == 0) {
+    $_SESSION['error_no_email'] = True;
     header('location:store_login.php');
-  }
+} else {
+    while($result_users = mysqli_fetch_array($query_users)){
+        if($password == $result_users['password']) {
+                $_SESSION['logged_in'] = True;
+                $_SESSION['logged_in_user'] = $result_users['email'];
+                header('location:store_index.php');
+            } else {
+                $_SESSION['error_wrong_password'] = True;
+                header('location:store_login.php');
+            }
+     
+    }
+
 }
-} // this closes the condition that checks if there were any results. 
-?>
+
+

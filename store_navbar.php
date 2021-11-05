7d5bbee55a2b09468740233472d8508649a82f6b
Bill MacKenty <bmackenty@users.noreply.github.com>
2021-11-05 10:17:01 +0100

    Update store_navbar.php

--- store_navbar.php
+++ store_navbar.php
@@ -1,49 +1,67 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-<!-- this file should be named store_navbar.php --> 
-        <a class="navbar-brand" href="index.php">ASW School Store</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div id="navbarNavDropdown" class="navbar-collapse collapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="store_control_panel.php">Control Panel</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav">
-                <?php
-                session_start();
-                $logged_in = $_SESSION['logged_in'];
-                $email = $_SESSION['logged_in_user'];
-                if($logged_in == True) {
-                    ?>
-                    <li class="nav-item">
-                        <a class="nav-link"><?php echo $email; ?></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="store_logout.php">Logout</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="store_cart.php">Shopping Cart</a>
-                    </li>
-                <?php
-                } else {
-                    ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="store_login.php">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="store_register.php">Register</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="store_cart.php">Shopping Cart</a>
-                </li>
-                    <?php
-                }
-                ?>
-            </ul>
-        </div>
-    </nav>
+<!-- this file should be named store_navbar -->
+<?php 
+session_start(); 
+?>
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #009900;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="store_index.php">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <?php 
+      if($_SESSION['logged_in']) {
+        ?>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link"><?php echo $_SESSION['logged_in_user']; ?></a>
+      </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="store_logout.php">Logout</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="store_control_panel.php">control panel</a>
+        </li>
+
+
+
+
+    </ul>
+    <?php 
+      } else {
+          ?>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="store_login.php">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="store_register.php">Register</a>
+        </li>
+    </ul>
+    <?php } ?>
+    
+    </div>
+  </div>
+</nav>

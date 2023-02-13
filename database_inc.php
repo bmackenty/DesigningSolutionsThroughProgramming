de90bd5c81cb2745d672ae6450461671ed04707f
Bill MacKenty <bmackenty@users.noreply.github.com>
2023-02-13 18:47:35 +0100

    Update database_inc.php

--- database_inc.php
+++ database_inc.php
@@ -7,9 +7,15 @@
 // 
 // to setup this file, please find the email I sent with your account information (not moodle)
 // you need to replace YOURUSERNAME with you username. You need to do this twice
-// you need to replace YOURPASSWORD with your password. You only need to do this once
+// you need to replace YOURPASSWORD with your password. You only need to do this once.
+// 
+// There is a conditional statement on line 17 which, if $connect is NOT TRUE
+// prints an error message helping us understand the reason why we could not connect 
+// to the database
 
 $connect = mysqli_connect("localhost","YOURUSERNAME","YOURPASSWORD","YOURUSERNAME");
-
+if (!$connect) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 ?>

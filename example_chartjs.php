b6e78e824817843fb6584131131a17e8ee583947
Bill MacKenty <bmackenty@users.noreply.github.com>
2023-02-27 11:03:19 +0100

    Create example_chartjs.php

---
+++ example_chartjs.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- this file is used to test and demonstrate chart.js  -->
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title>Example chart.js</title>
+</head>
+
+<body>
+    <?php
+    include('store_navbar.php');
+    include('database_inc.php');
+    ?>
+    <div class="container mt-5"> <!-- opening container div -->
+
+
+
+    <div>
+  <canvas id="myChart"></canvas>
+</div>
+
+
+
+
+    </div> <!-- closing container div -->
+    <?php
+    include('store_footer.php');
+    ?>
+    <!-- please don't touch anything below this line -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+</script>
+    
+
+</body>
+
+</html>

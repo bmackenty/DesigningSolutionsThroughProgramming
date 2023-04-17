63245630063783fc3afa410b80478d64edef980e
Bill MacKenty <bmackenty@users.noreply.github.com>
2023-04-17 10:16:17 +0200

    Update forum_create.php

--- forum_create.php
+++ forum_create.php
@@ -86,8 +86,7 @@ if (mysqli_num_rows($query_get_all_forums) != 0){
     <!-- =========================================== -->
 <!-- please don't touch anything below this line --> 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-</body>
-</html>
+
     <?php
     // This code displays every defined variable and error message.
     // It's quite helpful when you are debugging. 
